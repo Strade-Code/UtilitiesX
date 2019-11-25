a518b0dbde6c10779b5dc6a1265620da95bb10f9
Strade-Code <58144428+Strade-Code@users.noreply.github.com>
2019-11-25 10:05:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord =require(`discord.js`);
 const token = `NjQ4MjkyMTU2NDgxNzMyNjI5.XdvazQ.JKeN_jO5f7KsPxJYCH59kkbIYO8`;
-const prefix = "+";
+const prefix = "$";
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const fs = require('fs');
@@ -9,7 +9,7 @@ client.on(`ready`, () => {
   console.log(`Bot is now connected`);
   client.user.setPresence({
         game: {
-            name: 'Wumpus | Type +help',
+            name: 'Wumpus | Type $help',
             type: 'WATCHING'
         },
         status: 'online'
